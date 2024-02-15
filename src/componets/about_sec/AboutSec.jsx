1bc39f3f55fe8de360f852df5749a1a0588287f0
MWaqasMW <mwaqas6001@gmail.com>
2024-02-15 11:51:41 +0500

    add About section

--- src/componets/about_sec/AboutSec.jsx
+++ src/componets/about_sec/AboutSec.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./about_sec.css"
+import Button from '../button/Button'
+import { MdArrowOutward } from 'react-icons/md'
+import { IoArrowForward } from "react-icons/io5";
 const AboutSec = () => {
-  return (
-    <div>AboutSec</div>
-  )
+    return (
+        <div className='about_sec_main'>
+            <div className="container">
+                <div className="heading">About ME</div>
+                <div className="about_detail">
+                    <div className="head">I am a front-end developer based in Sydney. Has Mechanical Engineering background. </div>
+                    <p>I am a front-end developer based in Sydney looking for exciting opportunities. Has Mechanical Engineering background. Likes to focus on accessibility when developing. Passionate and curious about solving problems. Currently, I’m exploring Reactjs, Webflow and a bit of Designing. While I am not programming, I enjoy playing football, photography and playing Valorant. Learning more to improve skill.</p>
+                </div>
+                <div className="more_link">
+
+                    <span className="d-flex gap-2 align-items-center f-4">
+                        MORE ABOUT ME
+                        <div className="fs-4">
+                            <IoArrowForward />
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AboutSec
