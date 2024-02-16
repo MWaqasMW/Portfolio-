b77ee5f6ec5eaad2c8a45999df49f90ff563cfb1
MWaqasMW <mwaqas6001@gmail.com>
2024-02-16 14:28:50 +0500

    add Review section

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -4,12 +4,14 @@ import ContactSec from "@/componets/contect_sec/ContactSec"
 import Footer from "@/componets/nav/footer/Footer"
 import Header from "@/componets/nav/header/Header"
 import Projects from "@/componets/projects/Projects"
+import Review from "@/componets/reviews/Review"
 
 const page = () => {
   return (
     <>
       <Header />
       <Projects />
+      <Review />
       <ContactSec />
       <Footer />
     </>
