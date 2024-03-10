d11d1237d96f5c84d39648db718bdc5429ffd7ca
MWaqasMW <mwaqas6001@gmail.com>
2024-03-10 14:42:17 +0500

    update url icon and ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,10 +7,16 @@ import Header from "@/componets/nav/header/Header";
 import Footer from "@/componets/nav/footer/Footer";
 import Arrow from "@/componets/arrow/Arrow";
 
+export const metadata = {
+  title: "MWaqas",
+  description: "Muhammad Waqas MERN Stack Developer Portfolio",
+};
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className="main_body">
+        <link rel="icon" href="/icons/log.png" sizes="any" />
         <Header />
         {children}
         <Arrow />
