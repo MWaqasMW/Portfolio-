097a3764942f692cdb90e65fac1d388eccca5853
MWaqasMW <mwaqas6001@gmail.com>
2024-08-12 11:11:05 +0500

    Build Deploy

--- src/componets/skill/Skill.jsx
+++ src/componets/skill/Skill.jsx
@@ -14,8 +14,8 @@ const Skill = () => {
         { name: "Next js" },
         { name: "Node.js" },
         { name: "Express.js" },
-        { name: "Nest.js" },
         { name: "MongoDB" },
+        { name: "React Native" },
         // Add more skills as needed
     ];
 
@@ -33,7 +33,7 @@ const Skill = () => {
                             Muhammad Waqas is a skilled Frontend & MREN Stack Developer with expertise in a wide range of technologies. His capabilities include proficiency in HTML, CSS, Bootstrap, Tailwind CSS, JavaScript, TypeScript, React.js, Next.js, Node.js, Express.js, Nest.js, and MongoDB. With a focus on crafting user-friendly and responsive web interfaces, Waqas excels in integrating RESTful APIs and leveraging state management solutions like Redux/Context API.
                         </div>
                         <div className="watermarks">
-                            {skills.map((skill, index) => (
+                            {skills?.map((skill, index) => (
                                 <span key={index}>{skill.name}</span>
                             ))}
                         </div>
